48e5b61115741085cb3984c8c4547aea5ac9a9c4
David Axelrod <daxaxelrod@gmail.com>
2016-09-04 15:54:50 -0400

    management essay

---
+++ 9-2-16.py
@@ -0,0 +1,33 @@
+Marketing 311
+9-2-16
+
+selling experiences is much more important than selling the product
+
+forging a mission statement
+    what is out business
+    who is the customer
+    what do customers value
+    what should our business be?
+
+Steps om strategic planning
+    balancing act of corporate objects to business unit and market level
+
+STP Analysis
+    Segment
+    Target
+    Position
+
+Pestle  analysis
+    how the org is effected by outside forces
+        political
+        tech
+        legal
+        environmental
+        political
+        economic
+BCG matrix
+    dogs, stars, cash cows and question marks
+
+GE/ McKinsey matrix
+    just look it up
+    just another matrix
