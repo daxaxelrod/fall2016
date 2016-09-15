093dfae723df2a64d94c4f9ac5c6539e0c182107
David Axelrod <daxaxelrod@gmail.com>
2016-09-14 22:47:47 -0400

    notes up to 9/14/16

---
+++ Finance 311/9-14-16.py
@@ -0,0 +1,51 @@
+Fin 311
+9-14-16
+
+Quiz Monday
+from 8 am to 11 pm
+
+Standardizing financial statements
+    for balance sheet divide by total assets
+    for income statement divide by total sales
+
+5 types of ratios
+    Short term solvency ratios
+    long term solvency ratios
+    asset management ratios (asset utilization ratios)
+    profitability ratios
+    market value ratios
+
+liquidity ratios
+    current ratios
+        current assets/ current liabilities
+    quick ratio
+        buying inventory with cash drags quick down
+        current assets - inventory / current liabilities1
+
+leverage ratios
+    total debt ratio
+         TA-TE / TA
+    debt to equity
+        Total debt/ total equity
+    Equity multiplier
+        a function of the debt to equity ratio
+        Total assets/ total equity
+    LT debt ratio
+        LT debt / LT debt + Total equity
+    interest coverage ratio
+        EBIT/ interest
+    Cash coverage ratio
+        EBIT + Depreciation / Interest
+
+Asset turnover ratios
+    inventory turnover
+        Cogs/ inventory
+    Days' sales in inventory'
+        365 / inventory turnover
+    Reveivables turnover
+        sales/ Accounts Reveivable
+    Days' sales in receivables'
+        365/ AR
+    Payables turnover
+        Total supplier purchases/ average accounts Payable
+        
---
+++ Marketing 311/9-14-16.py
@@ -0,0 +1,53 @@
+Marketing 311
+9-14-16.py
+
+Types of buying desisions
+    Limited
+    habitual
+    Extended
+
+STM - Segmentation, Targeting and positioning
+    Segmentation:
+        1. Starts with Strategy or objectives
+        2. Segmentation mehtods
+    Targeting:
+        3. Evaluate Segment Attractiveness
+        4. Select Target Market
+    Positioning:
+        5. Identidy and develop positioning strategy
+
+    All Leads to marketing mix
+
+Geographic Segmentation
+    Location
+        placement of distro centers
+        placement of sales reps and office
+    Weather
+        Cue walmart weather analysis
+    Population Density
+    Topography
+    Origin
+        Miami FL is more spanish than NYC
+        Can overshadow location
+
+Demographic Segmentation
+    Most Common Strategy -- easiest to do
+        Gender
+        age
+        ethnicity
+
+Psychographic Segmentation
+    lifestyles
+    values
+    Self-concept
+
+Product Related Segmentation
+    benefits sought
+        eg people doing yoga want relaxation and to sit on a mat for a bit
+    Brand Loyalty
+    Occasion
+
+Met with group
+    zach from elc
+    girl named jen
+    some other guy who's name 'i didnt catch
