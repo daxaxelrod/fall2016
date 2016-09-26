971d9ec81e25ad8996040bd9b77ab01f7a598d08
David Axelrod <daxaxelrod@gmail.com>
2016-09-26 19:41:44 -0400

    notes up to 9-26-16

--- International Business/9-26-16.py
+++ International Business/9-26-16.py
@@ -8,3 +8,60 @@ innovation seen first in the developing world before spreading to the industrial
 
 it is litterally 715 and we are still talking about the case
 Just look at the questions on the 3rd slide in class notes 9-26-16
+
+
+Exam 1
+    9 True false
+    35 MC questions
+    know b number
+
+    Terms and concepts are available on the slide
+    practice question on Exam
+
+    different globalization Terms
+        understand the concepts
+        xpats and international premiums
+
+        know NGOs, MNE and the like
+
+    Force Majuer
+        an earthquake, act of god.
+        gives you exception from contracts if it happens to you
+
+    key function of institutions is to reduce uncertainty
+
+    know pillars of institutions
+
+    Lingua franca - language of business
+
+    context cluster and dimmessions
+
+    hofstedes dimensions -- not too much on this
+
+    ethics
+        100% question on code of conduct
+    CSR
+
+    opportunism vs individualism
+        what kind of an opportunity would an opportunist see in a collectivist environment
+        ^^ that just feels bullshitty but probs gonna be a question
+        look at the pros and cons relating to opportunity
+
+    tangible and intangible assets
+    outsourcing and insourcing
+
+    upstream
+        r and d
+        design
+    downstream
+        final assembly
+        marketing
+    know obm odm and oem
+        talking about how much of a life cycle you do
+
+    VRIO framework
+        Internal analysis
+
+    comparative advantage based on VRIO
+
+    Exam next class
