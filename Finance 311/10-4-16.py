e43509637b2180a89398dc350fdc2870331b290c
David Axelrod <daxaxelrod@gmail.com>
2016-10-22 17:27:35 -0400

    notes up to 10/22/16

---
+++ Finance 311/10-4-16.py
@@ -0,0 +1,7 @@
+Finance 311
+10-4-16
+
+Test monday
+
+EAR is the rate that you actually get
+    think yeild of dividend even though you have compounded before
---
+++ Marketing 311/10-4-16.py
@@ -0,0 +1,19 @@
+Marketing 311
+10-4-16
+
+Going over gaps from last class
+
+Went over article about diet pepsi
+Talked about ecommerce
+
+Welcome back to school everybody
+
+New product creation
+    test the market
+    How many product lines are we going to have
+        distribution
+    image/positioning
+        branding
+    ideas and evaluations
+    Differentiation
+    
